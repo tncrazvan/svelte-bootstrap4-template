bde904230059f9e1fc13ee6e6238bc0b7148d44e
Razvan <tangent.jotey@gmail.com>
2020-05-11 02:35:43 +0200

    SearchBar added as shared component

--- src/page/Home.svelte
+++ src/page/Home.svelte
@@ -1,9 +1,10 @@
 <script>
+	import SearchBar from './../shared/component/input/SearchBar.svelte';
 	import FloatingRegistrationForm from './../shared/component/form/FloatingRegistrationForm.svelte';
 	import { fly } from 'svelte/transition';
 </script>
 <div transition:fly={{ x: -100, duration: 200 }} class="page">
 
-	<FloatingRegistrationForm image={"/assets/images/logo.png"} />
+	<SearchBar />
 
 </div>
