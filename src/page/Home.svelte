ffe22fe17ada4aa329d8dea80a0cea7a6e9ce0cc
Razvan <tangent.jotey@gmail.com>
2020-05-13 12:02:40 +0200

    update

--- src/page/Home.svelte
+++ src/page/Home.svelte
@@ -1,10 +1,16 @@
 <script>
 	import SearchBar from './../shared/component/input/SearchBar.svelte';
-	import FloatingRegistrationForm from './../shared/component/form/FloatingRegistrationForm.svelte';
 	import { fly } from 'svelte/transition';
+	let suggestions = [
+		{value:"Print"},
+		{value:"#asd"}
+	];
+	let tags = {
+		"#asd": [
+			{value:"asd"}
+		]
+	};
 </script>
 <div transition:fly={{ x: -100, duration: 200 }} class="page">
-
-	<SearchBar />
-
+	<SearchBar suggestions={suggestions} tags={tags}/>
 </div>
